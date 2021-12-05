a7e3a67d3575df776cc538b6972ea52be996bb5f
Janek Kupke <38261186+KupkeJanek@users.noreply.github.com>
2021-12-05 15:02:30 +0100

    material module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,8 +3,14 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
-const materialModules = [MatButtonModule, MatDialogModule, MatSnackBarModule];
+const materialModules = [
+  MatButtonModule,
+  MatDialogModule,
+  MatSnackBarModule,
+  MatProgressSpinnerModule,
+];
 @NgModule({
   declarations: [],
   imports: [CommonModule, ...materialModules],
